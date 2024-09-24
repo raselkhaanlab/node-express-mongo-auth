bddf7ac7c2d2a3b57a52bd587e423e95405e3365
Md Rasel khan <raselkhanlab@gmail.com>
2024-09-24 11:19:55 +0600

    fix: swagger doc load issue

--- config/swagger.js
+++ config/swagger.js
@@ -1,9 +1,10 @@
 import YAML from 'yamljs'; // To load YAML files
 import swaggerUi from 'swagger-ui-express';
 import { env } from './env.js';
-
+import path from 'path';
 // Load the Swagger YAML file
-const loadedSpec = YAML.load("../swagger/index.yaml");
+const swaggerDocPath = path.resolve("../swagger/index.yaml");
+const loadedSpec = YAML.load(swaggerDocPath);
 
 const swaggerSpec = {...loadedSpec, servers:[
    {
